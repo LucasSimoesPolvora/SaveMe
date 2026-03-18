aae433c4d7c0808a82864d4ecc59af18a8f4975b
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2026-03-18 15:32:36 +0100

    refactor(repoService): Create function to return if repo is initialized or not

--- SaveMe/Services/RepoService.cs
+++ SaveMe/Services/RepoService.cs
@@ -2,34 +2,54 @@ public class RepoService
 {
     public static void InitRepo()
     {
-        DirectoryInfo info = new(Directory.GetCurrentDirectory());
-        foreach(var file in info.GetDirectories()){
-            if(file.Name == ".sm"){
-                Console.WriteLine("Repository already exists...\n" + 
-                                "This will delete all existing snapshots and data in the repository.\n" + 
-                                "Would you like to overwrite it? (y/n)");
-                if(Console.ReadKey().KeyChar == 'y'){
-                    Directory.Delete(Directory.GetCurrentDirectory() + "\\.sm", true);
-                    CreateRepo();
-                } else{
-                    Console.WriteLine("\nInitialization cancelled");
-                    return;
-                }
+        if (IsRepoInitialized())
+        {
+            Console.WriteLine("Repository already exists...\n" +
+                            "This will delete all existing snapshots and data in the repository.\n" +
+                            "Would you like to overwrite it? (y/n)");
+            if (Console.ReadKey().KeyChar == 'y')
+            {
+                Directory.Delete(Directory.GetCurrentDirectory() + "\\.sm", true);
+                CreateRepo();
+            }
+            else
+            {
+                Console.WriteLine("\nInitialization cancelled");
+                return;
             }
         }
-        CreateRepo();
-        Console.WriteLine("\nRepository created successfully.");
+        else
+        {
+            CreateRepo();
+            Console.WriteLine("\nRepository created successfully.");
+        }
     }
-    public static void CreateRepo(){
-        try{
+    public static void CreateRepo()
+    {
+        try
+        {
             Directory.CreateDirectory(".sm");
             _ = new DirectoryInfo(Directory.GetCurrentDirectory() + "\\.sm")
             {
                 Attributes = FileAttributes.Hidden
             };
-            
-        } catch(Exception ex){
+
+        }
+        catch (Exception ex)
+        {
             Console.WriteLine($"Error creating repository: {ex.Message}");
         }
+    }
+    public static bool IsRepoInitialized()
+    {
+        DirectoryInfo info = new(Directory.GetCurrentDirectory());
+        foreach (var file in info.GetDirectories())
+        {
+            if (file.Name == ".sm")
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
